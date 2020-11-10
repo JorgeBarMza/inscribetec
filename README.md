98442b913165eb1911e223cfba5f46927b9a9321
Jorge Barrios <jorgebarmza@gmail.com>
2020-11-09 20:55:59 -0600

    Update Readme

--- README.md
+++ README.md
@@ -4,15 +4,15 @@
 https://inscribetec-388b8.web.app
 
 ## Sample credentials
-Email: A01020781@itesm.mx    
-Password: 12345678   
+Email: `A01020781@itesm.mx`    
+Password: `12345678`   
 
-# Development environment workflow
+# Development environment setup
 
-npm install -g firebase-tools  
-firebase login  
-firebase login:ci  
-firebase projects:list  
-firebase init   
-firebase login --reauth  
-firebase deploy  
+    npm install -g firebase-tools   
+    firebase login    
+    firebase init   
+
+# Deployment
+
+`firebase deploy`   
--- app/js/schedule.js
+++ app/js/schedule.js
@@ -1,26 +1,11 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyArwHppjqEH7gOsqNS9un100EYkrF5cGiM",
-//   authDomain: "inscribetec-388b8.firebaseapp.com",
-//   databaseURL: "https://inscribetec-388b8.firebaseio.com",
-//   projectId: "inscribetec-388b8",
-//   storageBucket: "inscribetec-388b8.appspot.com",
-//   messagingSenderId: "455796643953",
-//   appId: "1:455796643953:web:c838f9d88af917770fe3d0",
-//   measurementId: "G-VD1C1VWBJ5"
-// };
-// firebase.initializeApp(firebaseConfig);
-
 function getSchedule() {
-  alert('entered');
   // Create a reference with an initial file path and name
   firebase.storage().ref('schedule.jpg').getDownloadURL().then(function(url) {
     var img = document.getElementById('img');
     img.src = url;
-    alert('changed');
   }).catch(function(error) {
     // Handle any errors
-    alert('No schedule found');
+    console.log('No schedule found');
   });
 }
 
