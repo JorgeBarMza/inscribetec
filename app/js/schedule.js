bf05c0fe8daf697ae3ca1e7fd67d223872fda720
yannlelorier <yannllb97@gmail.com>
2020-11-17 16:58:57 -0600

    adding hardcoded version of table

--- app/js/schedule.js
+++ app/js/schedule.js
@@ -1,9 +1,10 @@
 function getSchedule() {
   // Create a reference with an initial file path and name
-  firebase.storage().ref('schedule.jpg').getDownloadURL().then(function(url) {
+  firebase.storage().ref('schedule.jpg').getDownloadURL().then(function (url) {
     var img = document.getElementById('img');
     img.src = url;
-  }).catch(function(error) {
+
+  }).catch(function (error) {
     // Handle any errors
     console.log('No schedule found');
   });
