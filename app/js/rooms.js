4d83fbfb7de3650c5c50434e3e3ee07cc120cb45
Jorge Barrios <jorgebarmza@gmail.com>
2020-11-17 22:33:24 -0600

    Fix corrupt file rooms.js

--- app/js/rooms.js
+++ app/js/rooms.js
@@ -2,16 +2,11 @@ function getRooms() {
   // Query DB.
   firebase.database().ref('/rooms').once('value').then(
     function (snapshot) {
-      // Iterate rooms.
       let rooms = snapshot.val();
       Object.keys(rooms).forEach((key) => {
-        var ul = document.getElementById("ul");
-        console.log("Here ", ul)
-        var li = document.createElement("li");
-        var btn = document.createElement("BUTTON");
-        // Display room.
-        btn.innerHTML = rooms[key];
-        btn.onclick = () => {window.location.href = "../schedule.html";};
+        // Display coursenames.
+        btn.innerHTML = key;
+        btn.onclick = () => {window.location.href = "../schedule.html?room=" + key;};
         li.appendChild(btn);
         ul.appendChild(li);
       });
