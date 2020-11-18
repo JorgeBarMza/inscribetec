7952df5c44b74f5601445ceda12a4beebe8e55ca
Jorge Barrios <jorgebarmza@gmail.com>
2020-11-17 22:37:41 -0600

    Fix improved. (previous one failed)

--- app/js/rooms.js
+++ app/js/rooms.js
@@ -2,9 +2,13 @@ function getRooms() {
   // Query DB.
   firebase.database().ref('/rooms').once('value').then(
     function (snapshot) {
+      // Iterate rooms.
       let rooms = snapshot.val();
       Object.keys(rooms).forEach((key) => {
-        // Display coursenames.
+        var ul = document.getElementById("ul");
+        var li = document.createElement("li");
+        var btn = document.createElement("BUTTON");
+        // Display room.
         btn.innerHTML = key;
         btn.onclick = () => {window.location.href = "../schedule.html?room=" + key;};
         li.appendChild(btn);
