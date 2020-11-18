d9af8874e5c8185b9b3d165b11eccd74281a4c86
Jorge Barrios <jorgebarmza@gmail.com>
2020-11-17 21:54:36 -0600

    Make schedule dynamic.

--- app/js/schedule.js
+++ app/js/schedule.js
@@ -1,13 +1,23 @@
 function getSchedule() {
-  // Create a reference with an initial file path and name
-  firebase.storage().ref('schedule.jpg').getDownloadURL().then(function (url) {
-    var img = document.getElementById('img');
-    img.src = url;
-
-  }).catch(function (error) {
-    // Handle any errors
-    console.log('No schedule found');
-  });
+  let room = new URLSearchParams(window.location.search).get("room");
+  firebase.database().ref('/rooms/'+room).once('value').then(
+    function (snapshot) {
+      let week = ["monday", "tuesday", "wednesday", "thursday", "friday", 
+                  "saturday"];
+      let times = ["7", "8:30", "10", "11:30", "13", "14:30", "16", "17:30", "19", "20:30"];
+      let schedule = snapshot.val();
+      // Iterate cells.
+      for (i = 0; i < 60; ++i) {
+        let cell = document.getElementById(i);
+        let day = week[i % 6];
+        let dailySchedule = schedule[day];
+        if (!dailySchedule) continue;
+        let time = times[Math.floor(i / 6)];
+        let course = dailySchedule[time];
+        if (!course) continue;
+        cell.innerHTML = course;
+      }
+    });
 }
 
 window.onload = getSchedule
